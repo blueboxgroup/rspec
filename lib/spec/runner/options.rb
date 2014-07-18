9bfc8a0eb2a1e5c4611f1b38c79af22a3f2f32c6
Jesse Proudman <jproudman@bluebox.net>
2014-07-18 08:17:18 -0700

    Update options.rb
    Fix deprecation notice.
    
    rspec-1.3.2/lib/spec/runner/options.rb:188: Use RbConfig instead of obsolete and deprecated Config.

--- lib/spec/runner/options.rb
+++ lib/spec/runner/options.rb
@@ -185,7 +185,8 @@ module Spec
 
       def colour=(colour)
         @colour = colour
-        if @colour && Config::CONFIG['host_os'] =~ /mswin|mingw/ ;\
+        # if @colour && Config::CONFIG['host_os'] =~ /mswin|mingw/ ;\
+        if @colour && RbConfig::CONFIG['host_os'] =~ /mswin|mingw/ ;\
           begin ;\
             replace_output = @output_stream.equal?($stdout) ;\
             require 'rubygems' unless ENV['NO_RUBYGEMS'] ;\
